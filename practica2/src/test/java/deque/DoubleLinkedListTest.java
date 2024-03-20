48fc7e3837098101d1f20afafaa1383650c8e397
SimulationOfMario <imthedarkestspawn@gmail.com>
2024-03-20 10:19:20 +0100

    DoubleLinkedList fixed and completed

--- practica2/src/test/java/deque/DoubleLinkedListTest.java
+++ practica2/src/test/java/deque/DoubleLinkedListTest.java
@@ -1,5 +1,20 @@
 package deque;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 public class DoubleLinkedListTest {
+
+    DoubleLinkedList<Integer> dll;
+
+    @BeforeEach
+    public void setup()
+    {
+        dll = new DoubleLinkedList<Integer>();
+    }
+
 
 }
